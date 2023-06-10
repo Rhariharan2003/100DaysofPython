fbb230ff629e9c585795a5468af78cd8b9bdeb89
Rhariharan2003 <136108485+Rhariharan2003@users.noreply.github.com>
2023-06-10 13:34:45 +0530

    1.Heads_or_tails.py

---
+++ Day 04/1.Heads_or_tails.py
@@ -0,0 +1,12 @@
+#day04ofpython
+
+#Remember to use the random module
+#Hint: Remember to import the random module here at the top of the file. 🎲
+	 
+#Write the rest of your code below this line 👇
+import random
+ra = random.randint(0,1)
+if ra == 1:
+    print("Heads")
+else:
+     print("Tails")
