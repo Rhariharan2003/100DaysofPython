0e0965cd5a97718e5482f3c03064d44fbb0da908
Rhariharan2003 <136108485+Rhariharan2003@users.noreply.github.com>
2023-06-10 13:02:30 +0530

    1.Data_Types.py

---
+++ Day 02/1.Data_Types.py
@@ -0,0 +1,9 @@
+#day02ofpython
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+print(int(two_digit_number [0])+int(two_digit_number [1]))
