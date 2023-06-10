e51a9ed8b3d4facb957a62aa785be871cdf8e580
Rhariharan2003 <136108485+Rhariharan2003@users.noreply.github.com>
2023-06-10 13:04:31 +0530

    3.Life_in_weeks.py

---
+++ Day 02/3.Life_in_weeks.py
@@ -0,0 +1,10 @@
+#day02ofpython
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+a = 90 - int(age)
+
+print(f"You have {a*365} days, {a*52} weeks, and {a*12} months left.")
